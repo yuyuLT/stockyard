531d1b46b1c466d3950fbf63161d98d4f1b77673
yuyuLT <wawawawadaiko@icloud.com>
2024-11-02 12:32:50 +0900

    add thumbnails

--- themes/stockyard/functions.php
+++ themes/stockyard/functions.php
@@ -11,3 +11,5 @@ function register_menu()
     register_nav_menu('header-menu', __('header-menu', 'theme-slug'));
 }
 add_action('after_setup_theme', 'register_menu');
+
+add_theme_support('post-thumbnails');
